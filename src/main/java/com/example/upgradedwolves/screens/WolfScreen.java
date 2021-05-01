654d395393c9d61ae21459578df7d479f6d77789
Scott-GH <thatguywhowas@live.com>
2021-04-30 23:09:17 -0700

    Added wolf Screen

--- src/main/java/com/example/upgradedwolves/screens/WolfScreen.java
+++ src/main/java/com/example/upgradedwolves/screens/WolfScreen.java
@@ -1,13 +1,20 @@
 package com.example.upgradedwolves.screens;
 
+import com.example.upgradedwolves.UpgradedWolves;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+
+import java.awt.*;
 
 public class WolfScreen extends ContainerScreen<WolfContainer> {
-
+    private static final ResourceLocation TEXTURE = UpgradedWolves.getId("gui/wolf_chest_gui.png");
 
     public WolfScreen(WolfContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
         super(screenContainer, inv, titleIn);
@@ -15,12 +22,38 @@ public class WolfScreen extends ContainerScreen<WolfContainer> {
         this.guiTop = 0;
         this.xSize = 352;
         this.ySize = 334;
+    }    
+    @Override
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground(matrixStack);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
+        this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int x, int y) {
+        final float PLAYER_LABEL_XPOS = 8;
+        final float PLAYER_LABEL_DISTANCE_FROM_BOTTOM = (96 - 2);
+
+        final float BAG_LABEL_YPOS = 6;
+        //TranslationTextComponent bagLabel = new TranslationTextComponent(StartupCommon.itemFlowerBag.getTranslationKey());
+        StringTextComponent bagLabel = new StringTextComponent("Wolf Inventory");
+        float BAG_LABEL_XPOS = (xSize / 2.0F) - this.font.getStringWidth(bagLabel.getString()) / 2.0F;                  // centre the label
+        this.font.func_243248_b(matrixStack, bagLabel, BAG_LABEL_XPOS, BAG_LABEL_YPOS, Color.darkGray.getRGB());            //this.font.drawString;
+
+        float PLAYER_LABEL_YPOS = ySize - PLAYER_LABEL_DISTANCE_FROM_BOTTOM;
+        this.font.func_243248_b(matrixStack, this.playerInventory.getDisplayName(),                              //this.font.drawString;
+        PLAYER_LABEL_XPOS, PLAYER_LABEL_YPOS, Color.darkGray.getRGB());
     }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(MatrixStack matrixStack, float partialTicks, int x, int y) {
-        // TODO Auto-generated method stub
+        RenderSystem.color4f(1f, 1f, 1f, 1f);
+        this.minecraft.getTextureManager().bindTexture(TEXTURE);
 
+        int edgeSpacingX = (this.width - this.xSize) / 2;
+        int edgeSpacingY = (this.height - this.ySize) / 2;
+        this.blit(matrixStack, edgeSpacingX, edgeSpacingY, 0, 0, this.xSize, this.ySize);
     }
 
     
