7307786d2d018392f145616f71dbdaf0031cfea3
Scott-GH <thatguywhowas@live.com>
2021-05-03 22:49:21 -0700

    Made wolves flee when they have low health

---
+++ src/main/java/com/example/upgradedwolves/entities/goals/FleeOnLowHealthGoal.java
@@ -0,0 +1,24 @@
+package com.example.upgradedwolves.entities.goals;
+
+import net.minecraft.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.CreatureEntity;
+
+public class FleeOnLowHealthGoal extends AvoidEntityGoal<MonsterEntity> {
+    float minHealth;
+
+    public FleeOnLowHealthGoal(CreatureEntity entityIn, float avoidDistanceIn,
+            double farSpeedIn, double nearSpeedIn,float minHealth) {
+        super(entityIn, MonsterEntity.class, avoidDistanceIn, farSpeedIn, nearSpeedIn);
+        this.minHealth = minHealth;
+    }
+
+    @Override
+    public boolean shouldExecute() {
+        if(this.entity.getHealth() < minHealth && !(this.entity instanceof TameableEntity && ((TameableEntity)this.entity).isSitting()))
+            return super.shouldExecute();
+        return false;
+    }
+    
+}
