4c73e1703735f66e863d20b45f7994ccb724713e
Scott-GH <thatguywhowas@live.com>
2021-10-28 20:25:33 -0700

    Added message that creates a generic particle.

---
+++ src/main/java/com/example/upgradedwolves/network/message/CreateParticleForMobMessage.java
@@ -0,0 +1,58 @@
+package com.example.upgradedwolves.network.message;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraftforge.fmllegacy.network.NetworkEvent;
+
+public class CreateParticleForMobMessage implements IMessage<CreateParticleForMobMessage> {
+    protected int entityId;
+    protected ParticleOptions particleData;
+    protected int count;
+
+    public CreateParticleForMobMessage(){
+        entityId = 0;
+        count = 0;
+    }
+
+    public CreateParticleForMobMessage(int entityId,ParticleOptions particleType,int count){
+        this.entityId = entityId;
+        this.particleData = particleType;
+        this.count = count;
+    }
+
+    @Override
+    public void encode(CreateParticleForMobMessage message, FriendlyByteBuf buffer) {     
+        EntityDataSerializers.PARTICLE.write(buffer, message.particleData);
+        buffer.writeInt(message.entityId);
+        buffer.writeInt(message.count);
+    }
+
+    @Override
+    public CreateParticleForMobMessage decode(FriendlyByteBuf buffer) {
+        
+        ParticleOptions particleData = EntityDataSerializers.PARTICLE.read(buffer);
+        int entityId = buffer.readInt();
+        int count = buffer.readInt();        
+        return new CreateParticleForMobMessage(entityId,particleData,count);
+    }
+
+    @Override
+    public void handle(CreateParticleForMobMessage message, Supplier<NetworkEvent.Context> supplier) {
+        supplier.get().enqueueWork(() -> {
+            Minecraft mc = Minecraft.getInstance();
+            Entity entity = (Entity)mc.level.getEntity(message.entityId);
+            Random r = new Random();
+
+            for(int i = 0; i < message.count; i++)
+                mc.level.addParticle(message.particleData, false, entity.getPosition(1).x + r.nextDouble(), entity.getPosition(1).y + r.nextDouble(), entity.getPosition(1).z + r.nextDouble(), r.nextDouble()/5, r.nextDouble()/5, r.nextDouble()/5);
+        });
+
+    }
+
+}
